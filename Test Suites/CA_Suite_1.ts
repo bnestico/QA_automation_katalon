6bcbec66db0288ee1d81de851f6233575ff72e64
Rodrigo Davila <rdavila@consumeraffairs.com>
2018-09-10 10:35:05 -0300

    folder structure added
    folder structure added

--- Test Suites/CA_Suite_1.ts
+++ Test Suites/CA_Suite_1.ts
@@ -15,37 +15,37 @@
       <guid>1c1d6995-3445-4f74-b927-20dc3954f259</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/CA_Test_1</testCaseId>
+      <testCaseId>Test Cases/Sandbox/CA_Test_1</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>491ce563-9823-4e61-b631-abbcf952540f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/CA_Test_2</testCaseId>
+      <testCaseId>Test Cases/Sandbox/CA_Test_2</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>3d39a26f-7d67-4a73-a6c5-9b903a0d71cc</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/CA_Test_3</testCaseId>
+      <testCaseId>Test Cases/Sandbox/CA_Test_3</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>5e719586-b908-484b-9989-fa751ec31703</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/CA_Test_4</testCaseId>
+      <testCaseId>Test Cases/Sandbox/CA_Test_4</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>d27ea5ed-4364-4542-830e-9ac4e8d1682b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/CA_Test_5</testCaseId>
+      <testCaseId>Test Cases/Sandbox/CA_Test_5</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>c874c990-7220-4212-826c-4efce6686703</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/CA_Test_6</testCaseId>
+      <testCaseId>Test Cases/Sandbox/CA_Test_6</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>157ee443-93d0-420a-9258-d9cda48d1a17</guid>
